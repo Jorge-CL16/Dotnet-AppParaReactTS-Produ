2a4ed3b98a37162c6d8f81c71759acaa62631ac8
Jorge <jorge.medi.08@gmail.com>
2025-12-21 20:36:59 -0600

    Pirmer commit a produccion

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
-        "Freezing", "Prueba"
+        "Otro", "Prueba"
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
