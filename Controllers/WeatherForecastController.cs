3894fd344b42debad5f1a4dfb62948f7f0acbbfa
Jorge <jorge.medi.08@gmail.com>
2025-12-21 20:41:08 -0600

    Commit 2 a produccion

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
-        "Otro", "Prueba"
+        "Clima", "Prueba"
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
